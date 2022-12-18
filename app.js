7619f57d7c8a7f910ca3d91f6484a5f33dfc126f
Debashish Choudhury <1398deb@gmail.com>
2022-12-18 22:01:08 +0530

    port change

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.get('/about', (req, res) => {
 app.use('/ideas', ideas);
 app.use('/users', users);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
